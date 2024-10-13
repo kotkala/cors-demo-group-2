9c17f6614cf7794a2548907ce7dc027596a49081
kotkala <91318800+kotkala@users.noreply.github.com>
2024-10-14 02:04:47 +0700

    Demo

--- PageA/page/src/App.js
+++ PageA/page/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const fetchCount = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/count');
+        const data = await response.json();
+        setCount(data.count);
+      } catch (error) {
+        console.error('Error fetching count:', error);
+      }
+    };
+
+    fetchCount();
+    const intervalId = setInterval(fetchCount, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Trang A</h1>
+      <p>Số lần click: {count}</p>
     </div>
   );
 }
--- PageB/page/src/App.js
+++ PageB/page/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [lastClickCount, setLastClickCount] = useState(0);
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/click');
+      const data = await response.json();
+      setLastClickCount(data.count);
+    } catch (error) {
+      console.error('Error incrementing count:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Trang B</h1>
+      <button onClick={handleClick}>Click me!</button>
+      <p>Số lần click cuối cùng: {lastClickCount}</p>
     </div>
   );
 }
